195c3a3620f94f7a853c5cdec629c3ffa4fe9563
indrajeetM <indrajeetmarve1@gmail.com>
2020-04-21 20:06:59 +0530

    Updates

---
+++ precache-manifest.0ee5a13c15c0c56395ed31cb8bf465e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ff452abf678791972c5cd380a29fca",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6df8dc5de56b4f1e9779",
+    "url": "/my-app/static/css/main.80f5ad04.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/my-app/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df8dc5de56b4f1e9779",
+    "url": "/my-app/static/js/main.324d11fa.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  }
+]);
